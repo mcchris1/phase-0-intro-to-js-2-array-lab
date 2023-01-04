6ce2e1cc58e9afca16330caa654678f279e29a0c
mcchris1 <121463561+mcchris1@users.noreply.github.com>
2023-01-04 02:38:25 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat() {
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat() {
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+function appendCat() {
+    const Cats = [...cats, "Broom"];
+    return Cats;
+}
+function prependCat() {
+    const Cats = ["Arnold", ...cats]
+    return Cats;
+}
+function removeLastCat() {
+    const Cats = cats.slice(0, 2);
+    return Cats;
+}
+function removeFirstCat() {
+    const Cats = cats.slice (1);
+    return Cats;
+}
